890d943c31991404d8071dcf80a588e40e27c1ab
Frank Bodholdt Jakobsen <frankbj@bbsyd.dk>
2025-03-12 11:41:53 +0100

    Updated zoom code, simplified calculations

--- PgeMandelbrot1/PgeMandelbrot1.cpp
+++ PgeMandelbrot1/PgeMandelbrot1.cpp
@@ -27,18 +27,29 @@ public:
 		sAppName = "PgeMandelbrot1";
 	}
 
+private:
+	olc::vd2d worldOffset;
+	double worldScale;
+
 public:
-	bool OnUserCreate() override
+	void ResetView()
 	{
-		// Called once at the start, so create things here
-		// Setup mapping between pixels to complex number space
-		// Make room for the 2 x 2 area
+		// Setup mapping between pixels and complex number space
+		// Make room for the area [-2, 2] to [2,-2]
 		worldScale = 4.0 / std::max(ScreenWidth(), ScreenHeight());
 		// Make sure (0.0 , 0.0 ) is in the middle of the screen
 		worldOffset = {
 			-worldScale * ScreenWidth() / 2,
-			worldScale * ScreenHeight() / 2
+			// Pixel coordinates have increasing Y going down, Mandelbrot world have Y going up
+			-(-worldScale * ScreenHeight() / 2)
 		};
+	}
+
+	bool OnUserCreate() override
+	{
+		// Called once at the start, so create things here
+
+		ResetView();
 		maxCount = 256;
 		return true;
 	}
@@ -58,13 +69,7 @@ public:
 		if (GetKey(olc::Key::R).bPressed)
 		{
 			// Reset Screen-to-World transformation
-			// Make room for the 2 x 2 area
-			worldScale = 4.0 / std::max(ScreenWidth(), ScreenHeight());
-			// Make sure (0.0 , 0.0 ) is in the middle of the screen
-			worldOffset = {
-				-worldScale * ScreenWidth() / 2,
-				worldScale * ScreenHeight() / 2
-			};
+			ResetView();
 			maxCount = 256;
 		}
 
@@ -83,7 +88,6 @@ public:
 		}
 		if (GetKey(olc::Key::DOWN).bPressed)
 		{
-			// Screen coordinates have increasing Y going down
 			worldOffset.y -= worldScale * ScreenHeight() * 0.1;
 		}
 
@@ -91,46 +95,53 @@ public:
 		// Can also be checked for being held or released
 		if (GetMouse(olc::Mouse::LEFT).bPressed)
 		{
-			// Get mouse position and move the rectangle there
+			// Get mouse position and move the center of the rectangle there
 			olc::vi2d mouse = GetMousePos();
-			double x = worldOffset.x + mouse.x * worldScale;
-			double y = worldOffset.y - mouse.y * worldScale;
+			double x = worldOffset.x + mouse.x * worldScale - ScreenWidth()/2 * worldScale;
+			double y = worldOffset.y - mouse.y * worldScale + ScreenHeight()/2 * worldScale;
 
 			worldOffset = { x,y };
 		}
 
 		// Poll mouse wheel
-		if (GetMouseWheel() > 0)
+		// If scrolling, zoom around the mouse position
+		int32_t scroll = GetMouseWheel();
+		if (scroll != 0)
 		{
-			// Zoom in
-			worldScale *= 0.9;
-		}
-		else if (GetMouseWheel() < 0)
-		{
-			// Zoom out
-			worldScale *= 1.1;
+			olc::vi2d mouse = GetMousePos();
+			double oldScale = worldScale;
+
+			if (scroll > 0)
+			{
+				// Zoom in
+				worldScale *= 0.9;
+			}
+			else 
+			{
+				// Zoom out
+				worldScale *= (1.0 / 0.9); // To make zoom in and out reversible
+			}
+
+			// Adjust offset to zoom around mouse
+			double x = worldOffset.x + mouse.x * oldScale - mouse.x * worldScale;
+			double y = worldOffset.y - mouse.y * oldScale + mouse.y * worldScale;
+
+			worldOffset = { x,y };
 		}
 
 		// Clearing is not necessary in this example, since we redraw all pixels
 		// But this is how it's done
 		Clear(olc::BLACK);
 
-		// Screen y grows going down
-		olc::vi2d pixelTopLeft{ 0,0 };
-		olc::vi2d pixelBottomRight{ ScreenWidth(), ScreenHeight() };
-
-		// Current world view, should be calculated
+		// Current area for calculation must be calculated
 		olc::vd2d worldTopLeft = worldOffset;
-		olc::vd2d worldBottomRight;
-		worldBottomRight.x = worldOffset.x + worldScale * ScreenWidth();
-		worldBottomRight.y = worldOffset.y - worldScale * ScreenHeight();
 
-		double xStep = (worldBottomRight.x - worldTopLeft.x) / ScreenWidth();
-		double yStep = (worldBottomRight.y - worldTopLeft.y) / ScreenHeight();
+		double xStep = worldScale;
+		double yStep = -worldScale;
 
 		// Calculate and draw line by line
 		double worldY = worldTopLeft.y;
-		for (int y = pixelTopLeft.y; y < pixelBottomRight.y; y++)
+		for (int y = 0; y < ScreenHeight(); y++)
 		{
 			double worldX = worldTopLeft.x;
 			for (int x = 0; x < ScreenWidth(); x++)
@@ -178,9 +189,6 @@ private:
 		return count;
 	}
 
-	olc::vd2d worldOffset;
-	double worldScale;
-
 	int maxCount;
 
 	const float pi = 3.141593f;
@@ -195,6 +203,7 @@ int main()
 	std::cout << "Copyright 2018 - 2024 OneLoneCoder.com" << std::endl;
 	std::cout << "Copyright 2024 - Frank B. Jakobsen" << std::endl;
 	std::cout << "Color palette courtesy of @Eriksonn" << std::endl;
+
 	if (demo.Construct(640, 480, 2, 2, false, false, false, false))
 		demo.Start();
 
