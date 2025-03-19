2c5f61bde9b5b8a81e7781f6172746133a87cf49
Frank Bodholdt Jakobsen <frankbj@bbsyd.dk>
2025-03-19 11:33:36 +0100

    Basic single threaded method in place

--- PgeMandelbrotParallel/PgeMandelbrotParallel.cpp
+++ PgeMandelbrotParallel/PgeMandelbrotParallel.cpp
@@ -13,7 +13,7 @@ public:
 	}
 
 private:
-	// This extension will register itself to the 
+	// This extension will register itself to the current PGE object
 	olc::TransformedView tv;
 
 	void ResetView()
@@ -21,10 +21,10 @@ private:
 		// Scale is number of pixels per 1(one) world unit
 		// We want the complete circle with radius 2 to be visible, as this
 		// contains the complete Mandelbrot set
-		// Goes from upper left (-2, 2) to lower right (2, -2) in Mandelbrot world
+		// Goes from upper left (-2, 1.5) to lower right (1.0, -1.5) in Mandelbrot world
 		// Get the smallest scale to fit it all
 		float scale =
-			std::min<float>(ScreenWidth() / (4.0), ScreenHeight() / (4.0));
+			std::min<float>(ScreenWidth() / (3.0), ScreenHeight() / (3.0));
 		// World y and screen y goes in opposite directions, therefore the negative scale for y
 		tv.Initialise(
 			{ ScreenWidth(), ScreenHeight() },
@@ -34,11 +34,97 @@ private:
 		tv.SetWorldOffset(-(tv.ScreenToWorld(olc::vf2d{ (float) ScreenWidth() / 2, (float) ScreenHeight() / 2 })));
 	}
 
+	// Define a type for a drawing function
+	using DrawFunction = void ();
+
+	// Define a struct for information about a draw function
+	struct DrawFunctionDescription
+	{
+		olc::Key commandKey;		// Key to select this calculation method
+		std::string commandKeyName;	// Name of Key for display
+		std::string description;	// Description of this draw function
+		DrawFunction PgeMandelbrotParallel::* pDrawFunction;
+									// Pointer to the draw function as a member method
+	};
+
+	// Vector of information for the draw functions to be investigated
+	// Initialized at the bottom of this file
+	static std::vector<DrawFunctionDescription> DrawFunctions;
+
+	// Index of the currently selected draw function
+	size_t nCurrentDrawFunctionIndex = 0;
+
+	// The Mandelbrot algorithm
+	int32_t maxCount;  // Max count for the iterative function
+	const float pi = std::acos(-1.0F);
+	const float pithird = pi / 3;
+
+
+	int MandelbrotCount(double x, double y)
+	{
+		double zx = x;
+		double zy = y;
+		double zx2 = zx * zx;
+		double zy2 = zy * zy;
+
+		int count = 0;
+
+		while (count < maxCount && zx2 + zy2 <= 4.0)
+		{
+			zy = zy * zx * 2 + y;
+			zx = zx2 - zy2 + x;
+			zx2 = zx * zx;
+			zy2 = zy * zy;
+
+			count++;
+		}
+
+		return count;
+	}
+
+	void SingleThreadDrawing()
+	{
+		// Current area for calculation must be calculated
+		olc::vd2d worldTopLeft = tv.GetWorldOffset();
+		olc::vd2d worldScale = tv.GetWorldScale();
+
+		double xStep = 1.0/worldScale.x;
+		double yStep = 1.0/worldScale.y;
+
+		// Calculate and draw line by line
+		double worldY = worldTopLeft.y;
+		for (int y = 0; y < ScreenHeight(); y++)
+		{
+			double worldX = worldTopLeft.x;
+			for (int x = 0; x < ScreenWidth(); x++)
+			{
+				int count = MandelbrotCount(worldX, worldY);
+				olc::Pixel currPix;
+				if (count >= maxCount)
+					currPix = olc::BLACK;
+				else
+				{
+					float angle = 2 * pi * count / maxCount;
+					// Palette based on @Eriksonn's calculation, see my post and OneLoneCoder Discord channel
+					currPix = olc::PixelF(0.5f * sin(angle) + 0.5f, 0.5f * sin(angle + 2 * pithird) + 0.5f, 0.5f * sin(angle + 4 * pithird) + 0.5f);
+				}
+
+				Draw(x, y, currPix);
+				worldX += xStep;
+			}
+			worldY += yStep;
+		}
+	}
+
+
+
 public:
 	bool OnUserCreate() override
 	{
 		// Called once at the start, so create things here
 		ResetView();
+
+		maxCount = 256;
 
 		return true;
 	}
@@ -71,8 +157,7 @@ public:
 		// Clear, even if we redraw all pixels
 		Clear(olc::BLACK);
 
-		for (float radius = 0; radius <= 20; radius++)
-			tv.DrawCircle(0, 0, radius/10);
+		SingleThreadDrawing();
 
 		// Text output will be overlayed on the graphics
 		uint32_t textScale = 1;
@@ -89,10 +174,15 @@ public:
 };
 
 
+std::vector<PgeMandelbrotParallel::DrawFunctionDescription> PgeMandelbrotParallel::DrawFunctions 
+{
+
+};
+
 int main()
 {
 	PgeMandelbrotParallel engine;
-	if (engine.Construct(1280, 960, 1, 1))
+	if (engine.Construct(640*3/2, 480*3/2, 1, 1))
 		engine.Start();
 
 	return 0;
